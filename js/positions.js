007ffb27a85e18dda6ffbd3dce5d4c4fa814a31f
Yakuzi666 <47276749+Yakuzi666@users.noreply.github.com>
2023-03-31 12:14:48 +0200

    Added passed bool

--- js/positions.js
+++ js/positions.js
@@ -5,45 +5,55 @@ const posArray =
     //Reihe 1
   {
     name: ['01_Die_Ortsansaessigen','02_Steinbruch_Ort'],
-    coord: "48.131061,16.267094"
+    coord: "48.131061,16.267094",
+    passed: false
   },
   {
     name: ['07_Die_Einfahrt'],
-    coord: "48.130694,16.267080"
+    coord: "48.130694,16.267080",
+    passed: false
   },
   {
     name: ['03_Verladestation','04_Das_Lager'],
-    coord: "48.130333,16.267070"
+    coord: "48.130333,16.267070",
+    passed: false
   },
   {
     name: ['05_Die_Ortschaften','06_Die_Kirche'],
-    coord: "48.129890,16.267016"
+    coord: "48.129890,16.267016",
+    passed: false
   },
   //Reihe 2
   {
     name: ['24_Das_Lager_im_Westen'],
-    coord: "48.129432,16.267016"
+    coord: "48.129432,16.267016",
+    passed: false
   },
   {
     name: ['08_Die_Wirtschaft','13_Das_Ziegelwerk'],
-    coord: "48.128947,16.267169"
+    coord: "48.128947,16.267169",
+    passed: false
   },
   {
     name: ['15_Das_Steinbrecherhaus_Betrieb'],
-    coord: "48.128974,16.268046"
+    coord: "48.128974,16.268046",
+    passed: false
   },
   {
     name: ['33_Der_Landschaftsgarten','34_Die_Passionsspiele'],
-    coord: "48.129088,16.268818"
+    coord: "48.129088,16.268818",
+    passed: false
   },
   {
     name: ['31_Der_Badeunfall'],
-    coord: "48.129203,16.269612"
+    coord: "48.129203,16.269612",
+    passed: false
   },
   //Reihe 3
   {
     name: ['32_Die_Austrocknung_des_Teichs','11_Der_Baron'],
-    coord: "48.128950,16.270406"
+    coord: "48.128950,16.270406",
+    passed: false
   },
   {
     name: ['17_Das_NS-Arbeitsbuch_des_Steinbrucharbeiters','19_Die_Zwangsarbeit'],
@@ -51,43 +61,53 @@ const posArray =
   },
   {
     name: ['22_Der_Kellerausbau','23_Der_Aufseher'],
-    coord: "48.686426,15.855102"
+    coord: "48.686426,15.855102",
+    passed: false
   },
   {
     name: ['07_Die_Einfahrt','09_Die_Kollegen'],
-    coord: "48.686072,15.855005"
+    coord: "48.686072,15.855005",
+    passed: false
   },
   {
     name: ['27_Denunziert_und_deportiert','28_Die_Schotterwerbung'],
-    coord: "48.685856,15.854571"
+    coord: "48.685856,15.854571",
+    passed: false
   },
   //Reihe 4
   {
     name: ['29_Der_Badeteich','30_Der_Badeteich_und_das_Steinbrecherhaus'],
-    coord: "48.687120,15.853970"
+    coord: "48.687120,15.853970",
+    passed: false
   },
   {
     name: ['16_Das_Steinbrecherhaus_Stilllegung'],
-    coord: "48.687077,15.853421"
+    coord: "48.687077,15.853421",
+    passed: false
   },
   {
     name: ['35_Die_Wehrsportuebungen'],
-    coord: "48.686724,15.853362 "
+    coord: "48.686724,15.853362",
+    passed: false
   },
   {
     name: ['14_Das_Waechterhaus','12_Die_Betriebsbaracke'],
-    coord: "48.686369,15.853397"
+    coord: "48.686369,15.853397",
+    passed: false
   },
   {
     name: ['18_Der_Gefolgschaftsraum','20_Der_Betriebsfuehrer'],
-    coord: "48.686014,15.853445"
+    coord: "48.686014,15.853445",
+    passed: false
   },
   {
     name: ['21_Der_Nahrungsmangel','25_Vor_der_Befreiung'],
-    coord: "48.685660,15.853467"
+    coord: "48.685660,15.853467",
+    passed: false
   },
   {
     name: ['26_Tag_der_Befreiung','36_Rechtsradikale_Umtriebe'],
-    coord: "48.685348,15.853703"
+    coord: "48.685348,15.853703",
+    passed: false
   }
 ]
